f5a411efabb7675f33b8cf997072842b47b73e95
MauroFab <MauroFab@users.noreply.github.com>
2015-06-01 09:35:05 -0300

    Agregada clase unidad, con primera prueba pasada

---
+++ src/unidades/Unidad.java
@@ -0,0 +1,12 @@
+package unidades;
+
+public class Unidad {
+
+	public Unidad(int vidaMaxima) {
+	}
+
+	public boolean estaDestruida(){
+		return false;
+	}
+
+}
---
+++ test/unidades/UnidadTest.java
@@ -0,0 +1,16 @@
+package unidades;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnidadTest {
+	
+	@Test
+	public void alCrearseLaUnidadNoEstaDestruida(){
+		int vidaMaxima = 200;
+		Unidad unidad = new Unidad(vidaMaxima);
+		assertTrue(!unidad.estaDestruida());
+		
+	}
+}
