3d3fed8bf30bdfeb1b6ba3f7ebe004b263418b5f
MauroFab <MauroFab@users.noreply.github.com>
2015-06-01 00:53:54 -0300

    Cambiados test, por fallas locales estoy viendo siempre que paso las pruebas, este commit es una prueba rapida para probar poner una version estable

--- test/unidades/UnidadTest.java
+++ test/unidades/UnidadTest.java
@@ -4,13 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+@SuppressWarnings("unused")
 public class UnidadTest {
 
 	@Test
 	public void testLaUnidadAlCrearseNoEstaDestruida() {
 		int vidaMaxima = 10;
 		Unidad unidad = new Unidad(vidaMaxima);
-		assert(unidad.estaDestruida());
+		assert(!unidad.estaDestruida());
 	}
 
 }
