7f07e38c3170d9f39dc21c91447ad1d903d2ae5f
MauroFab <MauroFab@users.noreply.github.com>
2015-06-07 14:07:19 -0300

    Limpiada la clase jugador

--- src/juego/Jugador.java
+++ src/juego/Jugador.java
@@ -1,18 +1,5 @@
 package juego;
 
 public class Jugador {
-	static int mineralesIniciales = 200;
-	private int minerales;
-	
-	public Jugador() {
-		this.minerales = mineralesIniciales;
-	}
-	
-	public int getMinerales() {
-		return this.minerales;
-	}
-	
-	public void sumarMinerales(int mineralesRecolectados) {
-		this.minerales += mineralesRecolectados;
-	}
+
 }
