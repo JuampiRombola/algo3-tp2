720b9b8217f055a8faf9b58cc61ab9fff28f101c
MauroFab <MauroFab@users.noreply.github.com>
2015-06-08 16:55:16 -0300

    Creada la clase ConstructorDeUnidades para agrupar a los edificios de este tipo

---
+++ src/construcciones/ConstructorDeUnidades.java
@@ -0,0 +1,11 @@
+package construcciones;
+
+import mapa.Posicion;
+
+public abstract class ConstructorDeUnidades extends Edificio {
+
+	public ConstructorDeUnidades(int vida, Posicion posicion) {
+		super(vida, posicion);
+	}
+
+}
