0e31daf06af95c11fe12c4765bdd9b1d79641c70
MauroFab <MauroFab@users.noreply.github.com>
2015-06-09 13:40:51 -0300

    Borrada una copia de la interfaz Atacable que quedo suelta y sin sentido

--- src/Atacable.java
+++
@@ -1,9 +0,0 @@
-
-
-import algoCraft.mapa.Posicionable;
-
-public interface Atacable extends Posicionable{
-	boolean estaDestruido();
-	void recibePuntosDeDanio(int danio);
-	int getVidaActual();
-}
