14edf88b080e915897e50e13caeb304b50aa3089
MauroFab <MauroFab@users.noreply.github.com>
2015-06-28 23:02:06 -0300

    El menu funciona visualmente como se espera para la version final del juego

--- src/algoCraft/Vista/PanelBotonera.java
+++ src/algoCraft/Vista/PanelBotonera.java
@@ -1,14 +1,13 @@
 package algoCraft.Vista;
 
-import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
-import java.util.Observable;
-import java.util.Observer;
+
 import javax.swing.JPanel;
 
 import algoCraft.juego.Juego;
 
-public class PanelBotonera extends JPanel implements Observer{
+public class PanelBotonera extends JPanel{
 
 	private static final long serialVersionUID = 1L;
 	
@@ -16,46 +15,39 @@ public class PanelBotonera extends JPanel implements Observer{
 	private PanelDeConstruccion panelDeConstruccionDeRecolectorDeMinerales;
 
 	private PanelDeConstruccionDeRecolectorDeGas panelDeConstruccionDeRecolectorDeGas;
+
+	private String identificadorPanelEdificioProduccion = "PRODUCCION";
+	private String identificadorPanelRecolectorMineral= "MINERAL";
+	private String identificadorPanelRecolectorGas = "GAS";
+	private CardLayout layout;
 	
 	PanelBotonera(Juego juego){
-		juego.addObserver(this);
+		layout = new CardLayout();
+		this.setLayout(layout);
 		this.setBackground(Color.black);
-		this.setLayout(new BorderLayout());
 		this.setForeground(Color.white);
 		panelDeConstruccionDeEdificiosDeProduccion = new PanelDeConstruccionDeEdificiosDeProduccion();
-		panelDeConstruccionDeEdificiosDeProduccion.setVisible(false);
-		this.add(panelDeConstruccionDeEdificiosDeProduccion, BorderLayout.NORTH);
+		this.add(panelDeConstruccionDeEdificiosDeProduccion, identificadorPanelEdificioProduccion);
 		panelDeConstruccionDeRecolectorDeMinerales = new PanelDeConstruccionDeRecolectorDeMinerales();
-		panelDeConstruccionDeRecolectorDeMinerales.setVisible(false);
-		this.add(panelDeConstruccionDeRecolectorDeMinerales, BorderLayout.CENTER);
+		this.add(panelDeConstruccionDeRecolectorDeMinerales, identificadorPanelRecolectorMineral);
 		panelDeConstruccionDeRecolectorDeGas = new PanelDeConstruccionDeRecolectorDeGas();
-		panelDeConstruccionDeRecolectorDeGas.setVisible(false);
-		this.add(panelDeConstruccionDeRecolectorDeGas, BorderLayout.EAST);
+		this.add(panelDeConstruccionDeRecolectorDeGas, identificadorPanelRecolectorGas);
 		this.setVisible(false);
-		
 	}
 	
 	public void adecuarseParaFabricarEdificiosDeProduccion(){
-		panelDeConstruccionDeRecolectorDeGas.setVisible(false);
-		this.panelDeConstruccionDeRecolectorDeMinerales.setVisible(false);
-		this.panelDeConstruccionDeEdificiosDeProduccion.setVisible(true);
-		
+		this.setVisible(true);
+	    layout.show(this, identificadorPanelEdificioProduccion);
 	}
 
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		this.setVisible(true);
-	}
 
 	public void adecuarseParaFabricarRecolectorDeMineral() {
-		panelDeConstruccionDeRecolectorDeGas.setVisible(false);
-		this.panelDeConstruccionDeEdificiosDeProduccion.setVisible(false);
-		this.panelDeConstruccionDeRecolectorDeMinerales.setVisible(true);
+		this.setVisible(true);
+	    layout.show(this, identificadorPanelRecolectorMineral);
 	}
 
 	public void adecuarseParaFabricarRecolectorDeGas() {
-		this.panelDeConstruccionDeEdificiosDeProduccion.setVisible(false);
-		this.panelDeConstruccionDeRecolectorDeMinerales.setVisible(false);
-		panelDeConstruccionDeRecolectorDeGas.setVisible(true);
+		this.setVisible(true);
+	    layout.show(this, identificadorPanelRecolectorGas);
 	}
 }
