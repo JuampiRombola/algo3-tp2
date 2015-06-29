59c872ffa2f1baed6159295801438ec7c95aa9ef
MauroFab <MauroFab@users.noreply.github.com>
2015-06-29 03:01:40 -0300

    Me olvide un par de adds

--- src/Controlador/AccionTerminarTurno.java
+++ src/Controlador/AccionTerminarTurno.java
@@ -1,4 +1,4 @@
-package algoCraft.Vista;
+package Controlador;
 
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
@@ -10,7 +10,7 @@ public class AccionTerminarTurno extends AbstractAction{
 	private static final long serialVersionUID = 1L;
 	private Juego juego;
 
-	AccionTerminarTurno(Juego juego){
+	public AccionTerminarTurno(Juego juego){
 		super("Terminar turno");
 		this.juego = juego;
 		
