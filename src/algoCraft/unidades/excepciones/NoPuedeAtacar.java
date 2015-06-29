f98589b9b5d818d5b769fdac27d864253caeea52
JuampiRombola <juampi.rombola@gmail.com>
2015-06-29 15:12:58 -0300

    Subida excepcion nueva

---
+++ src/algoCraft/unidades/excepciones/NoPuedeAtacar.java
@@ -0,0 +1,12 @@
+package algoCraft.unidades.excepciones;
+
+import algoCraft.AlgoCraftException;
+
+public class NoPuedeAtacar extends AlgoCraftException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
